4bd46d66924fe73b2a4112afe5196ddc1e1c9462
EugeneNikiforov <115535800+EugeneNikiforov@users.noreply.github.com>
2023-04-16 15:02:43 +0300

    test changed

--- controllers/auth.test.js
+++ controllers/auth.test.js
@@ -1,16 +1,17 @@
 const mongoose = require('mongoose');
 const request = require('supertest');
+const gravatar = require('gravatar');
 require('dotenv').config();
 
 const app = require('../app');
-const { User } = require('../models/user');
+const User = require('../models/user');
 const { DB_HOST, PORT } = process.env;
 
-describe("test auth controller", () => {
+describe("test auth controllers", () => {
     let server;
     beforeAll(() => { server = app.listen(PORT) });
     afterAll(() => { server.close() });
-
+ 
     beforeEach((done) => {
         mongoose.connect(DB_HOST).then(() => { done() });
     })
@@ -20,11 +21,12 @@ describe("test auth controller", () => {
         })
     })
 
-    test('test login', async () => {
+    test('test login controller', async () => {
         const newUser = {
             password: "234567",
             email: "ashiok@teros.com",
-            subscription: "pro"
+            subscription: "pro",
+            avatarURL: gravatar.url("ashiok@teros.com")
         };
         const user = await User.create(newUser);
         const loginUser = {
@@ -32,15 +34,17 @@ describe("test auth controller", () => {
             email: "ashiok@teros.com"
         };
 
-        const response = await request(app).post("/users/login").send(loginUser);
+        const response = await request(app).post("/api/auth/users/login").send(loginUser);
         expect(response.statusCode).toBe(200);
         const { body } = response;
+        const { token } = await User.findById(user._id);
         expect(body.token).toBeTruthy();
+        expect(body.token).toBe(token);
         expect(typeof body.token).toBe("string");
-        expect(typeof body.User).toBe("object");
-        expect(body.User.email).toBeTruthy();
-        expect(typeof body.User.email).toBe("string");
-        expect(body.User.subscription).toBeTruthy();
-        expect(typeof body.User.subscription).toBe("string");
-    });
+        expect(typeof body.user).toBe("object");
+        expect(body.user.email).toBeTruthy();
+        expect(typeof body.user.email).toBe("string");
+        expect(body.user.subscription).toBeTruthy();
+        expect(typeof body.user.subscription).toBe("string");
+    }, 20000);
 })
